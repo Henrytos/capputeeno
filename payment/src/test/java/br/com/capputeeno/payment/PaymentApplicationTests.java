48119cdd221635ba5c30c47bc4a2b66b8b40e557
Henrytos <henry.arcaya@sptech.school>
2026-02-24 10:01:54 -0300

    primeiro commit do projeto

---
+++ payment/src/test/java/br/com/capputeeno/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.capputeeno.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ product/src/test/java/br/com/capputeeno/product/MsProductApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.capputeeno.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
